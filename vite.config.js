c20b0e3e592379ab7d28fa0ce3c8e5286c72f1e1
alternock <montilla269@gmail.com>
2026-07-14 07:41:49 +0200

    fix rename base vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/github.com/alternock/test_github_pages_2_react/'
+  base: '/test_github_pages_2_react/'
 })
